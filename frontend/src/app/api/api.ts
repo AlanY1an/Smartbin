ee49d5f0426574d1e15f00f75d38523876ff4310
AlanY1an <an1154811577@gmail.com>
2025-03-04 17:28:13 -0500

    “p

--- frontend/src/app/api/api.ts
+++ frontend/src/app/api/api.ts
@@ -3,7 +3,9 @@ import { API_BASE_URL, API_ENDPOINTS } from "./endpoints";
 export const apiRequest = async (endpoint: string, method: string, body: FormData | null = null) => {
     try {
         const response = await fetch(`${API_BASE_URL}${endpoint}`, { method, body });
-
+        console.log("API Base URL:", API_BASE_URL);
+        console.log("Full API URL:", `${API_BASE_URL}${API_ENDPOINTS.PREDICT}`);
+        
         let data;
         try {
             data = await response.json();

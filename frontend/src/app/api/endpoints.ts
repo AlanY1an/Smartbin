ba22ee1656cd7831d03851dc718f02fadef54930
AlanY1an <an1154811577@gmail.com>
2025-03-04 16:32:05 -0500

    update ip

--- frontend/src/app/api/endpoints.ts
+++ frontend/src/app/api/endpoints.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+export const API_BASE_URL = process.env.REACT_APP_API_URL || "13.59.8.90:8000";
 
 export const API_ENDPOINTS = {
     PREDICT: "/predict",
